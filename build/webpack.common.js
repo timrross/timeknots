bd4b76ccfd1e46cda0ee68c96928a3b2ba214eb4
Tim Ross <timrross@gmail.com>
2019-06-06 10:32:14 +0200

    Set up modern tooling.
    Use webpack, babel and eslint

---
+++ build/webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/timeknots.js',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'timeknots.js',
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+};
